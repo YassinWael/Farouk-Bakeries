bb4882d4861b3e7429e539fb83b8833fb8b6af33
YassinWael <yassinwaelmohamed84@gmail.com>
2024-10-18 20:15:47 +0300

    setup the database

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from pymongo import MongoClient
+from os import environ
+from dotenv import load_dotenv
+from icecream import ic
+load_dotenv()
 
 app = Flask(__name__)
+client = MongoClient(environ.get("mongodb"))
+db = client["favourites"]   
+favourites_collection = db["favourites"]
+
+favourites = (list(favourites_collection.find({})))
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/favourites",methods=["GET","POST"])
+def show_favourites():
+    return render_template("favourites.html",favourites=favourites)
 
 
 
